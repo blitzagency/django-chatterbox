0eed0bf0b739f7b40372d8933f812b674e171e71
Adam Venturella <aventurella@gmail.com>
2015-02-03 12:43:43 -0800

    enables simple_profile for instagram

--- chatterbox/api/instagram.py
+++ chatterbox/api/instagram.py
@@ -1,5 +1,22 @@
-from . import Oauth2Api
+from . import Oauth2Api, SimpleProfile
 
 
 class Instagram(Oauth2Api):
-    pass
+
+    def whoami(self):
+        return self.get("https://api.instagram.com/v1/users/self")
+
+    def simple_profile(self):
+        data = self.whoami()
+
+        data = data.get('data', {})
+
+        result = {
+            "id": data.get("id", None),
+            "name": data.get("username", None),
+            "link": "http://instagram.com/{}".format(data.get("username", None)),
+            "picture": data.get("profile_picture", None)
+        }
+
+        profile = SimpleProfile(**result)
+        return profile
