aa928500ca63618b854846da91a9a9434997e5a9
dinopetrone <dinopetrone@gmail.com>
2015-02-03 09:11:11 -0800

    initial setup for api object

--- chatterbox/api/__init__.py
+++ chatterbox/api/__init__.py
@@ -1,6 +1,29 @@
+import pytz
+from datetime import (datetime, timedelta)
+from requests_oauthlib import OAuth2Session
+
 
 class Api(object):
-    pass
+
+    def __init__(self, key, client):
+        # meant to be overriden
+        pass
+
+    def post(self, url, **kwargs):
+        r = self._session.request(
+            "POST",
+            url=url,
+            **kwargs
+        )
+        return r.json()
+
+    def get(self, url, **kwargs):
+        r = self._session.request(
+            "GET",
+            url=url,
+            **kwargs
+        )
+        return r.json()
 
 
 class Oauth1Api(Api):
@@ -8,19 +31,49 @@ class Oauth1Api(Api):
 
 
 class Oauth2Api(Api):
-    pass
+    def __init__(self, key, client):
 
-# this'll be key/client
-# def __init__(self, client_id, client_secret, token, token_updater=None):
+        self.key = key
+        self.client = client
 
-#         token_updater = token_updater or printing_token_saver
+        refresh_extras = {
+            "client_id": client.client_id,
+            "client_secret": client.client_secret,
+        }
 
-#         refresh_extras = {
-#             "client_id": client_id,
-#             "client_secret": client_secret,
-#         }
+        token = {
+            'access_token': key.access_token,
+            'token_type': client.driver.token_type,
+            'refresh_token': key.refresh_token,
+        }
 
-#         self._session = OAuth2Session(client_id, token=token,
-#             auto_refresh_url=self.refresh_url,
-#             auto_refresh_kwargs=refresh_extras,
-#             token_updater=token_saver)
+        self._session = OAuth2Session(
+            client.client_id,
+            token=token,
+            auto_refresh_url=client.driver.refresh_url,
+            auto_refresh_kwargs=refresh_extras,
+            token_updater=self.token_updater
+        )
+
+    def token_updater(self, token):
+
+        # save access_token
+        self.client.access_token = token.get('access_token')
+        self.client.save()
+
+        # save expires time
+        now = datetime.now(pytz.utc)
+        expires_at = now + timedelta(seconds=token.get('expires_in'))
+        self.key.expires = expires_at
+
+        # save refresh_token
+        self.key.refresh_token = token.get('refresh_token')
+        self.key.save()
+
+# example
+# {
+# u'access_token': u'ya29.CwFYP2TJGLjyscdPS85ZofQ8C88XbxH5fP--VupiFMDWoEerHnQ',
+# u'token_type': u'Bearer',
+# u'expires_in': 3600,
+# u'refresh_token': u'1/t2GZlVdGSO1cTGqPNC-ZX6ocAMEudVrK5jSpoR30zcRFq6',
+# u'expires_at': 1422628154.377442}
