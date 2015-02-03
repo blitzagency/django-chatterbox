be83a28fa6a78fa64145b11c1d1e42b1faf34e7a
Adam Venturella <aventurella@gmail.com>
2015-02-03 10:06:51 -0800

    implements required simple_profile method

--- chatterbox/api/youtube.py
+++ chatterbox/api/youtube.py
@@ -1,9 +1,22 @@
-from . import Oauth2Api
+from . import Oauth2Api, SimpleProfile
 
 
 class YouTube(Oauth2Api):
     def whoami(self):
-        return self.get('https://www.googleapis.com/oauth2/v2/userinfo')
+        return self.get("https://www.googleapis.com/oauth2/v2/userinfo")
+
+    def simple_profile(self):
+        data = self.whoami()
+
+        result = {
+            "id": data["id"],
+            "name": data["name"],
+            "link": data["link"],
+            "picture": data["picture"],
+        }
+
+        profile = SimpleProfile(**result)
+        return profile
 
     # def search(self, query):
     #     return self.get('asdf?{}'.format(query))
