d562ea9f0b5d9e424418d2179c5570ea7683bbd5
Adam Venturella <aventurella@gmail.com>
2015-02-03 10:07:43 -0800

    updates Key admin to display service user name and service user id

--- chatterbox/admin.py
+++ chatterbox/admin.py
@@ -9,7 +9,8 @@ class ClientAdmin(admin.ModelAdmin):
 
 @admin.register(Key)
 class KeyAdmin(admin.ModelAdmin):
-    list_display = ('get_service', 'get_client', 'user')
+    list_display = ('get_service', 'get_client', 'user',
+                    'get_service_username', 'get_service_user_id')
 
     def get_service(self, obj):
         return obj.service.label
@@ -17,12 +18,23 @@ class KeyAdmin(admin.ModelAdmin):
     def get_client(self, obj):
         return obj.client.label
 
+    def get_service_username(self, obj):
+        return obj.service_username
+
+    def get_service_user_id(self, obj):
+        return obj.service_user_id
 
     get_service.short_description = 'Service'
     get_service.admin_order_field = 'service__label'
 
     get_client.short_description = 'Client'
     get_client.admin_order_field = 'client__label'
+
+    get_service_username.short_description = 'Service User'
+    get_service_username.admin_order_field = 'service_username'
+
+    get_service_user_id.short_description = 'Service User ID'
+    get_service_user_id.admin_order_field = 'service_user_id'
 
 
 admin.site.register(Service)
