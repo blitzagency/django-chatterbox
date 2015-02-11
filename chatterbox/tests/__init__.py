93e306b3d21d9ca7ae8f0c746b48fad6cfc72f77
dinopetrone <dinopetrone@gmail.com>
2015-02-11 14:25:12 -0800

    turning tests on

--- chatterbox/tests/__init__.py
+++ chatterbox/tests/__init__.py
@@ -1,8 +1,8 @@
-# from .facebook import FacebookApiTestCase
-# from .instagram import InstagramApiTestCase
-# from .instagram import InstagramUtils
-# from .twitter import TwitterApiTestCase
-# from .twitter import TwitterUtils
-# from .youtube import YouTubeApiTestCase
-# from .facebook import FacebookUtils
+from .facebook import FacebookApiTestCase
+from .instagram import InstagramApiTestCase
+from .twitter import TwitterApiTestCase
+from .youtube import YouTubeApiTestCase
+from .instagram import InstagramUtils
+from .twitter import TwitterUtils
+from .facebook import FacebookUtils
 from .youtube import YoutubeUtils
