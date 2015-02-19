f298335e682d497fd0a15159fec277ed3d0e10f7
dinopetrone <dinopetrone@gmail.com>
2015-02-19 14:55:06 -0800

    done with twitter search unit tests

--- chatterbox/tests/test_collector_twitter.py
+++ chatterbox/tests/test_collector_twitter.py
@@ -47,3 +47,27 @@ class CollectorTwitterSearchTestCase(TransactionTestCase):
         job.key.api.search = mock.Mock(return_value=return_value)
         job.run()
         self.assertEqual(Activity.objects.all().count(), 1)
+
+    def test_multiple_job_tweets(self):
+        job1 = self.job
+
+        # mock the request response
+        return_value = load_json("twitter-basic-search-response")
+        job1.key.api.search = mock.Mock(return_value=return_value)
+        job1.run()
+
+        # create 2nd job
+        job2 = Job()
+        job2.collector = job1.collector
+        job2.key = job1.key
+        job2.data = {"tag": "#dino"}
+        job2.save()
+
+        return_value = load_json("twitter-basic-search-response")
+        job2.key.api.search = mock.Mock(return_value=return_value)
+        job2.run()
+
+        activity = Activity.objects.all()[0]
+        self.assertEqual(activity.job.all().count(), 2)
+
+        self.assertEqual(Activity.objects.all().count(), 2)
