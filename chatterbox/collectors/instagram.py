f2343dd13c5a24af5a3faa9e3ab05e5c831f6ff3
dinopetrone <dinopetrone@gmail.com>
2015-02-20 14:19:45 -0800

    instagram search collector done

--- chatterbox/collectors/instagram.py
+++ chatterbox/collectors/instagram.py
@@ -30,6 +30,10 @@ class InstagramUserForm(forms.Form):
     user_id = forms.CharField(label='User ID', max_length=100)
 
 
+class InstagramSearchForm(forms.Form):
+    query = forms.CharField(label='Search Term(s)', max_length=100)
+
+
 class InstagramWall(Collector):
     activity_from_dict = activity_from_dict
 
@@ -55,3 +59,27 @@ class InstagramWall(Collector):
 
 class InstagramWallFromKey(InstagramWall):
     form = InstagramUserForm
+
+
+class InstagramSearch(Collector):
+    activity_from_dict = activity_from_dict
+    form = InstagramSearchForm
+
+    def action(self, job):
+        log.debug("Invoking action")
+        job.data = job.data or {}
+        query = job.data.get('query')
+        api = job.key.api
+        statuses = instagram_iterator(api, api.search, query=query)
+        for status in statuses:
+            activity = self.create_or_get_activity_from_dict(status)
+            try:
+                self.register_activity(activity, job)
+            except Exception:
+                log.debug("Stopping action")
+                # need to decide when to stop...
+                # at this point this job has already processed this activity
+                # before, should we do nothing?  for now we are going to stop
+                # all iteration and be done, in the future we might want to
+                # keep running the process and run an update action? idk...
+                break
--- chatterbox/tests/test_collector_instagram.py
+++ chatterbox/tests/test_collector_instagram.py
@@ -10,7 +10,7 @@ class CollectorInstagrameWallTestCase(TransactionTestCase):
     fixtures = ('project/apps/chatterbox/fixtures/users.json',
                 'project/apps/chatterbox/fixtures/dump.json')
 
-    def setUp(self):
+    def test_basic_wall_scrape(self):
         service = Service.objects.get(key='instagram')
 
         collector = Collector()
@@ -26,23 +26,58 @@ class CollectorInstagrameWallTestCase(TransactionTestCase):
         job.collector = collector
         job.key = key
         job.save()
-        self.job = job
 
-    def test_basic_wall_scrape(self):
-        job = self.job
-        job.collector.driver = "chatterbox.collectors.instagram.InstagramWall"
         # mock the request response
         return_value = load_json("instagram-user-feed-response")
         job.key.api.user_media = mock.Mock(return_value=return_value)
         job.run()
+
         self.assertEqual(Activity.objects.all().count(), 2)
 
     def test_user_wall_scrape(self):
-        job = self.job
-        job.collector.driver = "chatterbox.collectors.instagram.InstagramWallFromKey"
+        service = Service.objects.get(key='instagram')
+
+        collector = Collector()
+        collector.driver = 'chatterbox.collectors.instagram.InstagramWall'
+        collector.service = service
+        collector.save()
+
+        client = service.keys.all()[0]
+
+        key = client.client_keys.all()[0]
+
+        job = Job()
+        job.collector = collector
+        job.key = key
+        job.save()
+
         # mock the request response
         return_value = load_json("instagram-user-feed-response")
         job.key.api.user_media = mock.Mock(return_value=return_value)
         job.data = {"user_id": "11314839"}
         job.run()
         self.assertEqual(Activity.objects.all().count(), 2)
+
+    def test_search_scrape(self):
+        service = Service.objects.get(key='instagram')
+
+        collector = Collector()
+        collector.driver = 'chatterbox.collectors.instagram.InstagramSearch'
+        collector.service = service
+        collector.save()
+
+        client = service.keys.all()[0]
+
+        key = client.client_keys.all()[0]
+
+        job = Job()
+        job.collector = collector
+        job.key = key
+        job.save()
+
+        # mock the request response
+        return_value = load_json("instagram-search-response")
+        job.key.api.search = mock.Mock(return_value=return_value)
+        job.data = {"query": "fireworks"}
+        job.run()
+        self.assertEqual(Activity.objects.all().count(), 20)
